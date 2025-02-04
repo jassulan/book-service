098eb99c87bb5ff3659a168cfdc6e8d26560fb7c
jassulan <jkaishentayev@motio.com>
2025-02-04 18:47:22 +0500

    init start.spring.io

---
+++ src/main/java/com/example/testrestexample/TestRestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.testrestexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRestExampleApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(TestRestExampleApplication.class, args);
+   }
+
+}
